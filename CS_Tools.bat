7c77f1a4c1f159f42423cd81bc4a8996325cb9e4
jay-hankins <jay.hankins@outlook.com>
2014-10-22 23:17:20 -0400

    Updated URLs

--- CS_Tools.bat
+++ CS_Tools.bat
@@ -1,11 +1,11 @@
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/jay-hankins/ChocolateyPackages/master/AutoInstallers/PowerShell Scripts/CSTools.ps1'))"
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/jay-hankins/Auto_Installers/master/PowerShell_Scripts/CS_Tools.ps1'))"
 
 cyg-get wget
 cyg-get curl
 cyg-get nano
 cyg-get vim
 
-wget https://raw.githubusercontent.com/jay-hankins/ChocolateyPackages/master/AutoInstallers/Shell%20Scripts/Oh-My-Cygwin-Downloader.sh
+wget https://raw.githubusercontent.com/jay-hankins/Auto_Installers/master/Shell_Scripts/Oh_My_Cygwin_Downloader.sh
 C:\bin\cygwin\bin\bash Oh-My-Cygwin-Downloader.sh
 del Oh-My-Cygwin-Downloader.sh
