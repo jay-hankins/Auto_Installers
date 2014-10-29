f5d8b8608c3277c97c80834d5b95953869a7509b
jay-hankins <jay.hankins@outlook.com>
2014-10-28 23:05:22 -0400

    More changing packages.

--- PowerShell_Scripts/CS_Tools.ps1
+++ PowerShell_Scripts/CS_Tools.ps1
@@ -4,7 +4,7 @@
 # Note: Net 4.0 must be installed prior to running this script
 #
 #Modify this line to change packages
-$items = @("jdk8", "javaruntime", "eclipse-standard-luna", "notepadplusplus.install", "cyg-get", "PuttyTray", "winscp", "wget", "ConEmu", "JayHankins.ConEmuConfig", "git.install")
+$items = @("jdk8", "javaruntime", "eclipse-java-luna", "notepadplusplus.install", "cyg-get", "PuttyTray", "winscp", "wget", "ConEmu", "JayHankins.ConEmuConfig", "git.install")
 
 
 
