0ce8e39664dc9b685d59d0f32d7ead931a7f227b
jay-hankins <jay.hankins@outlook.com>
2014-11-26 11:31:49 -0500

    Added BoxStarter

---
+++ BoxStarter/BS-Install.bat
@@ -0,0 +1,2 @@
+mkdir %systemdrive%\tools
+START http://boxstarter.org/package/nr/url?https://gist.githubusercontent.com/jay-hankins/117fc39fe76491c40dc6/raw/2109d21837144e30d8034230bbac35ff3cfdec94/Everyone-packages
