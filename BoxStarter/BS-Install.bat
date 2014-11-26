0f718961e75d0c07cba81e38e89f479fe397ea95
jay-hankins <jay.hankins@outlook.com>
2014-11-26 11:34:09 -0500

    updated http paths

--- BoxStarter/BS-Install.bat
+++ BoxStarter/BS-Install.bat
@@ -1,2 +1,2 @@
 mkdir %systemdrive%\tools
-START http://boxstarter.org/package/nr/url?https://gist.githubusercontent.com/jay-hankins/117fc39fe76491c40dc6/raw/2109d21837144e30d8034230bbac35ff3cfdec94/Everyone-packages
+START http://boxstarter.org/package/nr/url?https://raw.githubusercontent.com/jay-hankins/Auto_Installers/master/BoxStarter/Jay-Packages.txt
